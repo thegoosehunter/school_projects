059649e2c211808cbb8d24baad8bb68bdf6af04d
thegoosehunter <lessi@DESKTOP-D7D6UR2>
2018-08-30 16:54:12 -0700

    Initial upload

---
+++ Triangle/src/Triangle.java
@@ -0,0 +1,54 @@
+/**
+ * Triangle Problem
+<p>
+ * @author      Les Simmons
+ * @version     1.0
+ * @since       2018-27-08
+<p>
+ * Longer description.
+ * Program to test what type of a triangle intergers would create:
+ *  Equilateral, Isosceles, or Scalene  or if it is not a triangle 
+ * pulls data from input.txt
+*/
+import java.io.*;
+import java.util.*;
+
+public class Triangle {
+
+	  /**
+     * Short one line description of the method
+    <p>
+     * @author      Les Simmons
+     * @version     1.0
+     * @param args  a String array of commmand line parameters;
+     *              may be empty.
+     * @since       2018-27-08
+    <p>
+     * Longer description.
+     * The program loops to read each line of input and processes it in turn. 
+     * Use exception handling to trap input errors, and report them as "Not a Triangle"
+    */
+	
+	public static void main(String[] args) {
+		
+		int s1;
+		int s2;
+		int s3;
+		
+		// TODO Auto-generated method stub
+
+		// TODO Get data from file
+		
+		// TODO Test isTriangle
+		
+		// TODO Test isEquilateral 
+		
+		// TODO Test isIsosceles
+		
+		// TODO Test isScalene
+		
+		// TODO Output results
+		
+	}
+
+}
