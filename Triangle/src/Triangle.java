99494404d83c9de213585810a2b35b9d3cbe5306
thegoosehunter <lessi@DESKTOP-D7D6UR2>
2018-09-02 09:47:19 -0700

    Working copy

--- Triangle/src/Triangle.java
+++ Triangle/src/Triangle.java
@@ -2,8 +2,8 @@
  * Triangle Problem
 <p>
  * @author      Les Simmons
- * @version     1.0
- * @since       2018-27-08
+ * @version     1.0.4
+ * @since       2018-02-09
 <p>
  * Longer description.
  * Program to test what type of a triangle intergers would create:
@@ -14,7 +14,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.InputMismatchException;
-import java.util.ArrayList;
 
 public class Triangle {
 
@@ -22,10 +21,10 @@ public class Triangle {
      * Short one line description of the method
     <p>
      * @author      Les Simmons
-     * @version     1.0
+     * @version     1.0.4
      * @param args  a String array of commmand line parameters;
      *              may be empty.
-     * @since       2018-27-08
+     * @since       2018-02-09
     <p>
      * Longer description.
      * The program loops to read each line of input and processes it in turn. 
@@ -39,44 +38,49 @@ public class Triangle {
 		
 		
 		Scanner sc = new Scanner (new File(file));
-		sc.useDelimiter(" ");
 		
 		while (sc.hasNext() ) {
-		
-		int s1 = sc.nextInt();
-		int s2 = sc.nextInt();
-		int s3 = sc.nextInt();
-		
-		
-		if ((s1 > s2 + s3) || (s2 > s1 + s3) || (s3 > s1 + s2)) {
-			System.out.println("This is not a triangle because one side cannot be larger than the sum of the other sides.");
 			
-		}
-		
-		else if ((s1==0) || (s2==0) || (s3==0)) {
-			System.out.println("This is not a triangle because one side cannot be equal to zero.");
-		}
-		
-		else if ((s1==s2) && (s1==s3) && (s2==s3)) {
-			System.out.println("The triangle is Equalateral.");
-		}
-		
-		else if ((s1==s2) || (s2==s3) || (s1==s3)) {
-			System.out.println("The triangle is Isosceles.");
-		}
-		
-		else if ((s1==s2) && (s2==s3) && (s1==s3)) {
-			System.out.println("The triangle is Scalene.");
-		
-		}	
-		
-		else {
-			System.out.println("Something went wrong.");
+			String stringRead = sc.nextLine();
 			
-		}
+			Scanner parse = new Scanner (stringRead);
+			parse.useDelimiter(" ");
+			
 		
-	
-		}
+			int s1 = parse.nextInt();
+			int s2 = parse.nextInt();
+			int s3 = parse.nextInt();
+		
+			if ((s1==0) || (s2==0) || (s3==0)) {
+				System.out.println("This is not a triangle because one side cannot be equal to zero.");
+			}
+			
+			else if ((s1 > s2 + s3) || (s2 > s1 + s3) || (s3 > s1 + s2)) {
+				System.out.println("This is not a triangle because one side cannot be larger than the sum of the other sides.");
+			
+			}
+		
+		
+			else if ((s1==s2) && (s1==s3) && (s2==s3)) {
+				System.out.println("The triangle is Equalateral.");
+			}
+		
+			else if ((s1==s2) || (s2==s3) || (s1==s3)) {
+				System.out.println("The triangle is Isosceles.");
+			}
+		
+			else if ((s1!=s2) && (s2!=s3) && (s1!=s3)) {
+				System.out.println("The triangle is Scalene.");
+		
+			}	
+		
+			else {
+				System.out.println("Something went wrong.");
+			
+			}
+		
+		
+			}
 		
 		}
 		
@@ -92,6 +96,7 @@ public class Triangle {
 			System.out.println("This is not a triangle because one of the variables is not an interger.");
 			
 		}
+		
 		
 		
 	}
