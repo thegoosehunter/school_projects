780d54116a9fc1ade3a2688679cf72d3705917bc
thegoosehunter <lessi@DESKTOP-D7D6UR2>
2018-09-06 08:30:14 -0700

    initial upload

---
+++ ChernoffFaces/src/ChernoffFaces.java
@@ -0,0 +1,36 @@
+/**
+ * Short one line description of the class
+<p>
+ * @author      Les Simmons
+ * @version     1.0
+ * @since       2018-22-08
+<p>
+ * Longer description.
+ * Describe what the class does and how it works.
+*/
+import javax.swing.JApplet;
+import javax.swing.JOptionPane;
+import java.awt.Graphics;
+
+public class ChernoffFaces extends JApplet {
+
+	  /**
+     * Short one line description of the method
+    <p>
+     * @author      Les Simmons
+     * @version     1.0
+     * @param args  a String array of commmand line parameters;
+     *              may be empty.
+     * @since       2018-22-08
+    <p>
+     * Longer description.
+     * Describe what the method does and how it works.
+    */
+	
+	public void paint (g) {
+		
+		super.paint(g);
+		
+	}
+
+}
