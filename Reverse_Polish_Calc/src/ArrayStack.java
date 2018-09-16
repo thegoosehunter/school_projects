ec1bed3585f37178d3098f1b128927a4d7cc9ba5
thegoosehunter <lessi@DESKTOP-D7D6UR2>
2018-09-15 18:46:11 -0700

    initial upload

---
+++ Reverse_Polish_Calc/src/ArrayStack.java
@@ -0,0 +1,53 @@
+/**
+ * ArrayStack for Reverse Polish (HP) Style Calculator
+<p>
+ * @author      Les Simmons
+ * @version     1.0
+ * @since       2018-15-09
+<p>
+ * Longer description.
+ * Describe what the class does and how it works.
+*/
+public class ArrayStack {
+	 /**
+     * Short one line description of the method
+    <p>
+     * @author      Les Simmons
+     * @version     1.0
+     * @param args  a String array of commmand line parameters;
+     *              may be empty.
+     * @since       2018-15-09
+    <p>
+     * Longer description.
+     * Describe what the method does and how it works.
+    */
+	//ArrayStack constructor
+	public ArrayStack () {
+		
+	}
+	
+	//Push Method
+	public push () {
+		
+	}
+	
+	//Pop Method
+	public pop () {
+		
+	}
+	
+	//isEmpty Method
+	public isEmpty () {
+		
+	}
+	
+	//Peek Method
+	public peek () {
+		
+	}
+	
+	//Count Method
+	public count () {
+		
+	}
+}
---
+++ Reverse_Polish_Calc/src/TestArrayStack.java
@@ -0,0 +1,30 @@
+/**
+ * Test Client for ArrayStack
+<p>
+ * @author      Les Simmons
+ * @version     1.0
+ * @since       2018-15-09
+<p>
+ * Longer description.
+ * Describe what the class does and how it works.
+*/
+
+public class TestArrayStack {
+	 /**
+     * Short one line description of the method
+    <p>
+     * @author      Les Simmons
+     * @version     1.0
+     * @param args  a String array of commmand line parameters;
+     *              may be empty.
+     * @since       2018-15-09
+    <p>
+     * Longer description.
+     * Describe what the method does and how it works.
+    */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
