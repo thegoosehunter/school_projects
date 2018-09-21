1075df8429ad64f2b6d4e6a0274e735b3bdcefb8
thegoosehunter <lessi@DESKTOP-D7D6UR2>
2018-09-21 08:18:15 -0700

    added AbstractStack initial code

---
+++ Reverse_Polish_Calc/src/AbstractStack.java
@@ -0,0 +1,44 @@
+/**
+ * Test Client for ArrayStack
+<p>
+ * @author      Les Simmons
+ * @version     1.0.2
+ * @since       2018-16-09
+<p>
+ * Longer description.
+ * Describe what the class does and how it works.
+*/
+public abstract class AbstractStack {
+	/**
+     * Short one line description of the method
+    <p>
+     * @author      Les Simmons
+     * @version     1.0.2
+     * @param args  a String array of commmand line parameters;
+     *              may be empty.
+     * @since       2018-16-09
+    <p>
+     * Longer description.
+     * Describe what the method does and how it works.
+    */
+	
+	//push method
+	public abstract void push(double c) {
+		
+	}
+	//peek method
+	public abstract double peek() {
+		
+	}
+	
+	//pop method
+	public abstract double pop() {
+		
+	}
+	
+	//clear method
+	public abstract void clear() {
+		
+	}
+
+}
