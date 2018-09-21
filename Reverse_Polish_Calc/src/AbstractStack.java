2023b4c4062513001cb2f4ba0000b8dd09b83705
thegoosehunter <lessi@DESKTOP-D7D6UR2>
2018-09-21 08:26:10 -0700

    updated comments

--- Reverse_Polish_Calc/src/AbstractStack.java
+++ Reverse_Polish_Calc/src/AbstractStack.java
@@ -1,22 +1,22 @@
 /**
- * Test Client for ArrayStack
+ * AbstractStack
 <p>
  * @author      Les Simmons
  * @version     1.0.2
- * @since       2018-16-09
+ * @since       2018-21-09
 <p>
  * Longer description.
  * Describe what the class does and how it works.
 */
 public abstract class AbstractStack {
 	/**
-     * Short one line description of the method
+     * pure abstract class with methods
     <p>
      * @author      Les Simmons
      * @version     1.0.2
      * @param args  a String array of commmand line parameters;
      *              may be empty.
-     * @since       2018-16-09
+     * @since       2018-21-09
     <p>
      * Longer description.
      * Describe what the method does and how it works.
