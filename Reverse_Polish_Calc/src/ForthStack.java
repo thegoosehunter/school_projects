5d08032ad9c736273c6a01956cdf6fe4955b675e
thegoosehunter <lessi@DESKTOP-D7D6UR2>
2018-09-22 21:24:47 -0700

    updated forthstack

--- Reverse_Polish_Calc/src/ForthStack.java
+++ Reverse_Polish_Calc/src/ForthStack.java
@@ -1,4 +1,47 @@
 
-public class ForthStack extends ArrayStack {
+public class ForthStack extends ArrayStack implements Forth {
+
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sub() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mult() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void div() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dup() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void twoDup() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double peek() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
 
 }
