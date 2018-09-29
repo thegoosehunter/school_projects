0f1937756822d9998c0fab9ef6bd641ac3d49165
thegoosehunter <lessi@DESKTOP-D7D6UR2>
2018-09-29 08:33:59 -0700

    basic window

--- Reverse_Polish_Calc/src/RPN.java
+++ Reverse_Polish_Calc/src/RPN.java
@@ -1,8 +1,30 @@
+import javax.swing.JFrame;
+import java.awt.Container;
 
-public class RPN {
+public class RPN extends JFrame {
+	
+	//container
+	private Container contents;
+	
+	//constructor
+	public RPN(){
+		
+		//title bar
+		super("Reverse Polish Notation Calculator");
+		
+		//get container
+		contents = getContentPane();
+		
+		//window size
+		setSize(400,300);
+		
+		//make visible
+		setVisible(true);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		RPN rpnGui = new RPN();
+		rpnGui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	}
 
